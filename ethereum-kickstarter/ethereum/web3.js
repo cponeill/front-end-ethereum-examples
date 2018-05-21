0a32a44525d3a77c1faa88b0a619269dc32f9e59
Casey O'Neill <caseyoneill78@hotmail.com>
2018-05-20 17:14:47 -0700

    updating web3 to handle code with or without metamask.

--- ethereum-kickstarter/ethereum/web3.js
+++ ethereum-kickstarter/ethereum/web3.js
@@ -1,5 +1,17 @@
 import Web3 from 'web3';
 
-const web3 = new Web3(window.web3.currentProvider);
+let web3;
+
+if (typeof window !== 'undefined' && typeof window.web3 !== 'undefined') {
+	// handle code inside browser with metamask available
+	web3 = new Web3(window.web3.currentProvider);
+} else {
+	// we are on the server *OR* the user is not running metamask
+	const provider = new Web3.providers.HttpProvider(
+		'https://rinkeby.infura.io/Q2YzEPrszqdrnFf2uRoW'
+	);
+
+	web3 = new Web3(provider);
+}
 
 export default web3;
